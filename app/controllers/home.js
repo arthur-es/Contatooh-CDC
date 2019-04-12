93d3efc373523ded61764485133980af1b4e3ac3
= <arthurcastroes@gmail.com>
2019-04-12 10:47:00 -0300

    Added a lot of stuff

---
+++ app/controllers/home.js
@@ -0,0 +1,14 @@
+var controller = require('../controllers/home')
+module.exports = function() {
+    var controller = {};
+    controller.index = function(req, res){
+        //a view ainda nao ta pronta: tem que ser renderizada
+        res.render('index', {nome: 'Express App'}); //obj com as vars que a view vai consumir
+    };
+    return controller;
+}
+
+module.exports = function(app) {
+    app.get('/index', controller.index);
+    app.get('/', controller.index);
+}
--- config/express.js
+++ config/express.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var home = require('../app/routes/home')
 
 module.exports = function() {
     var app = express();
@@ -6,8 +7,15 @@ module.exports = function() {
     //variaveis de ambiente
     app.set('port', 3000);
 
+
+    //app.use(app.router); //necessario apenas na versao 3.x
+
     //middleware
-    app.use(express.static('./public'));
+    app.use(express.static('./public')); //recebe a pasta public
+    app.set('view engine', 'ejs');
+    app.set('views','./app/views'); // onde vai ficar as nossas views
+
+    home(app);
 
     return app;
 }
